fdbe01d4ac28a714becedf9d605388158633063b
Corentin Bonifacio <78152277+CorentinSIOdev@users.noreply.github.com>
2021-11-14 23:17:43 +0100

    Add files via upload

---
+++ migrations/Version20211111230030.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211111230030 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE client_role (client_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_86F5490819EB6921 (client_id), INDEX IDX_86F54908D60322AC (role_id), PRIMARY KEY(client_id, role_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, role_name VARCHAR(255) NOT NULL, status TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE client_role ADD CONSTRAINT FK_86F5490819EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE client_role ADD CONSTRAINT FK_86F54908D60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client_role DROP FOREIGN KEY FK_86F54908D60322AC');
+        $this->addSql('DROP TABLE client_role');
+        $this->addSql('DROP TABLE role');
+    }
+}
